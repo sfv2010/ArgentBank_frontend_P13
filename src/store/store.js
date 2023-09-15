3b81c77093942a03f02c4bb94d1988e72603fae7
Sayaka Vincent <sfv2010@hotmail.fr>
2023-09-15 14:08:21 +0200

    Removed the comment

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,4 @@ export const store = configureStore({
     reducer: {
         user: userSlice,
     },
-    // devTools:
-    //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    //     window.__REDUX_DEVTOOLS_EXTENSION__(),
 });
