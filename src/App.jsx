2a8881631cf4f5c1989773c988af44f32dfb4899
Sayaka Vincent <sfv2010@hotmail.fr>
2023-09-22 12:55:40 +0200

    Add Error404

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import Profile from "./pages/Profile/Profile";
+import Error404 from "./pages/Error/Error404";
 
 function App() {
     return (
@@ -14,7 +15,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<SignIn />} />
                 <Route path="/profile" element={<Profile />} />
-                <Route path="*" element={<Home />} />
+                <Route path="*" element={<Error404 />} />
             </Routes>
             <Footer />
         </BrowserRouter>
