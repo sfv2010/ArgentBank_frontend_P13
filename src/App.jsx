75c5242bcf86584126166a364213f41944d1d66c
Sayaka Vincent <sfv2010@hotmail.fr>
2023-10-15 21:57:22 +0200

    Removed App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
